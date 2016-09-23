c01227d05e14cae085aa962873729ffdd8665d4f
Jim Stys <jstys92@gmail.com>
2016-09-23 18:41:51 -0400

    Initial commit.  Removed unnecessary modules from the paho project.  Added a function to subscribe to a single topicFilter to the MqttService.  Added a basic tasker plugin wrapper that abstracts out some of the tasker plugin logic from the MqttService

---
+++ org.eclipse.paho.android.service/src/main/java/org/eclipse/paho/android/service/TaskerBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package org.eclipse.paho.android.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class TaskerBroadcastReceiver extends BroadcastReceiver {
+    public TaskerBroadcastReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ org.eclipse.paho.android.service/src/main/java/org/eclipse/paho/android/service/TaskerUtility.java
@@ -0,0 +1,8 @@
+package org.eclipse.paho.android.service;
+
+/**
+ * Created by jim.stys on 9/23/16.
+ */
+
+public class TaskerUtility {
+}
