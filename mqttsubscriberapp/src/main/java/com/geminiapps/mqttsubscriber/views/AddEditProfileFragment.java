c60a14daae4e6e84db3e3c34064c673b65d8fcbc
Jim Stys <jstys92@gmail.com>
2017-04-09 12:35:07 -0400

    Fix display issue when loading an existing profile into the popup dialog where the Protocol field would always show "TCP"

--- mqttsubscriberapp/src/main/java/com/geminiapps/mqttsubscriber/views/AddEditProfileFragment.java
+++ mqttsubscriberapp/src/main/java/com/geminiapps/mqttsubscriber/views/AddEditProfileFragment.java
@@ -2,6 +2,7 @@ package com.geminiapps.mqttsubscriber.views;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -41,14 +42,21 @@ public class AddEditProfileFragment extends DialogFragment {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context, R.array.protocol_values_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mBinding.protocolSpinner.setAdapter(adapter);
-        mBinding.protocolSpinner.setSelection(0);
 
         if (profile != null) {
             getDialog().setTitle("Edit Connection Profile");
             mBinding.profileNameEdittext.setEnabled(false);
+            Uri brokerUri = Uri.parse(profile.getBrokerUri());
+            if(brokerUri.getScheme().equalsIgnoreCase("ws")){
+                mBinding.protocolSpinner.setSelection(1);
+            }
+            else{
+                mBinding.protocolSpinner.setSelection(0);
+            }
         } else {
             getDialog().setTitle("Add Connection Profile");
             mBinding.profileNameEdittext.setEnabled(true);
+            mBinding.protocolSpinner.setSelection(0);
         }
 
         return mBinding.getRoot();
